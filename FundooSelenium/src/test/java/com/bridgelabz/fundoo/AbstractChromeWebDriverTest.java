2989b8a61d167b196bf5db6a8269e24c3205f15e
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-05-19 19:39:40 +0530

    [Brijesh]. Add. Implemented CreateLabel test , EditLabel Test and DeleteLabel test

--- FundooSelenium/src/test/java/com/bridgelabz/fundoo/AbstractChromeWebDriverTest.java
+++ FundooSelenium/src/test/java/com/bridgelabz/fundoo/AbstractChromeWebDriverTest.java
@@ -2,6 +2,8 @@ package com.bridgelabz.fundoo;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
@@ -9,15 +11,26 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class AbstractChromeWebDriverTest {
 
 	protected WebDriver driver;
+	protected ExtentHtmlReporter htmlReport;
+	protected ExtentReports extentReport;
+	protected ExtentTest extentTest;
 
 	public AbstractChromeWebDriverTest() {
 		super();
@@ -25,6 +38,24 @@ public abstract class AbstractChromeWebDriverTest {
 
 	@BeforeTest
 	public void beforeTest() {
+//		String url="D:\\FundooSelenium\\FundooSelenium\\test-output\\myReport.html";
+		htmlReport=new ExtentHtmlReporter(System.getProperty("user.dir")+ "/test-output/myReport.html");
+		htmlReport.config().setDocumentTitle("Automation Report");//Title of the Report
+		htmlReport.config().setReportName("Functional Report");//Name of the Report
+		htmlReport.config().setTheme(Theme.DARK);
+		
+		extentReport=new ExtentReports();
+		extentReport.attachReporter(htmlReport);
+		
+		//Set System Info
+		extentReport.setSystemInfo("Hostname", "LocalHost");
+		extentReport.setSystemInfo("OS", "Windows10");
+		extentReport.setSystemInfo("Browser", "Chrome");
+		
+		
+		
+		
+		
 		 //Download the WebDriver Executable
 		  WebDriverManager.chromedriver().setup();
 		  
@@ -36,8 +67,27 @@ public abstract class AbstractChromeWebDriverTest {
 
 	@AfterTest
 	public void afterTest() {
+		extentReport.flush();
 		  driver.quit();
 	  }
+	
+	@AfterMethod
+	public void testResult(ITestResult result) throws IOException
+	{
+		if(result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(Status.FAIL,"Test Case Failed Is" + result.getName()); //To Add Name in Extent Report
+			extentTest.log(Status.FAIL, "Test Case Failed Is" + result.getThrowable());//To Add Error/Exception in Extent Report
+			
+			String screenPath=screenCapture(result.getName());
+			extentTest.addScreenCaptureFromPath(screenPath);
+		}
+		else if (result.getStatus() == ITestResult.SUCCESS) 
+		{
+			extentTest.log(Status.PASS,"Test Case Passed Is " + result.getName());
+			String screenPath=screenCapture(result.getName());
+			extentTest.addScreenCaptureFromPath(screenPath);
+		}
+	}
 	
 	public void sleep(int seconds) {
 		try {
@@ -47,15 +97,28 @@ public abstract class AbstractChromeWebDriverTest {
 		}
 	}
 	static int i=1;
-	public void screenCapture(String methName)  {
-		File scrFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		File screenName=new File("D:\\FundooSelenium\\FundooSelenium\\screenshots\\"+methName+ i++ +".png");
-		 try {
-			FileUtils.copyFile(scrFile, screenName);
-		} catch (IOException e) {
+	public String screenCapture(String methName)  {
+		String dateName=new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File scrFile=ts.getScreenshotAs(OutputType.FILE);
+		
+		//"D:\\FundooSelenium\\FundooSelenium\\screenshots\\"+methName+ i++ + dateName +".png";
+		
+		String dest=System.getProperty("user.dir")+ "/screenshots/" +methName+ i++ + dateName +".png";
+		
+		File finalDestName=new File(dest);
+		 
+		try 
+		{
+			FileUtils.copyFile(scrFile, finalDestName);
+		} 
+		catch (IOException e) 
+		{
 			e.printStackTrace();
 		}
-		 Reporter.log("<br><img src='"+screenName+"' height='250' width='500' /></br>");
+		 
+//		Reporter.log("<br><img src='"+finalDestName+"' height='250' width='500' /></br>");
+		return dest;
 	}
 
 }
--- FundooSelenium/src/test/java/com/bridgelabz/fundoo/NoteService/NoteServiceTest.java
+++ FundooSelenium/src/test/java/com/bridgelabz/fundoo/NoteService/NoteServiceTest.java
@@ -29,8 +29,7 @@ public class NoteServiceTest extends AbstractChromeWebDriverTest {
 		WebElement loginElement = driver.findElement(By.id("submit"));
 		loginElement.click();
 		
-		sleep(10);
-		driver.get("http://localhost:4200/dashboard");
+		
 		driver.manage().window().maximize();
 		sleep(15);
 		
@@ -39,12 +38,12 @@ public class NoteServiceTest extends AbstractChromeWebDriverTest {
 		createElement.click();
 		
 		WebElement titleElement = driver.findElement(By.id("noteTitle"));
-		titleElement.sendKeys("Fourth Selenium Note");
+		titleElement.sendKeys("Sixth Selenium Note");
 		
 		sleep(2);
 		
 		WebElement descElement = driver.findElement(By.id("noteDesc"));
-		descElement.sendKeys("test Fourth Note");
+		descElement.sendKeys("test sixth Note");
 		
 		sleep(2);
 		screenCapture(new Exception().getStackTrace()[0].getMethodName());
@@ -62,6 +61,7 @@ public class NoteServiceTest extends AbstractChromeWebDriverTest {
 	
 	@Test
 	public void testUpdateNote() {
+		extentTest=extentReport.createTest("FundooUpdateNoteTest");
 		driver.get("http://localhost:4200/login");
 		
 		WebElement emailElement = driver.findElement(By.id("email"));
@@ -73,13 +73,12 @@ public class NoteServiceTest extends AbstractChromeWebDriverTest {
 		passwordElement.sendKeys("abcd");
 		
 		sleep(2);
-		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+//		screenCapture(new Exception().getStackTrace()[0].getMethodName());
 		WebElement submitElement = driver.findElement(By.id("submit"));
 		submitElement.click();
 		
-		sleep(10);
-		driver.get("http://localhost:4200/dashboard");
 		sleep(15);
+		
 		
 		driver.manage().window().maximize();
 		
@@ -91,22 +90,342 @@ public class NoteServiceTest extends AbstractChromeWebDriverTest {
 		
 		WebElement updateTitleElement = driver.findElement(By.id("title"));
 		updateTitleElement.clear();
-		updateTitleElement.sendKeys("Selenium Note title test");
+		updateTitleElement.sendKeys("Selenium Note Update ");
 		
 		WebElement descTitleElement = driver.findElement(By.id("desc"));
 		descTitleElement.clear();
-		descTitleElement.sendKeys("Selenium Note desc test");
+		descTitleElement.sendKeys("Note description Update");
 		
 		sleep(2);
-		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+//		screenCapture(new Exception().getStackTrace()[0].getMethodName());
 		WebElement closeElement = driver.findElement(By.id("close"));
 		closeElement.click();
 		
+//		sleep(10);
+//		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+	}
+	
+	@Test
+	public void testArchiveNote() {
+		
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		WebElement archiveIcon = driver.findElement(By.id("archiveIcon"));
+		archiveIcon.click();
+		
+		sleep(3);
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+		
+		WebElement sideNavElement = driver.findElement(By.id("sideMenu"));
+		sideNavElement.click();
+		
+		sleep(2);
+		
+		WebElement archiveSideNavElement = driver.findElement(By.id("onClickArchive"));
+		archiveSideNavElement.click();
+		
+		sleep(3);
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+	}
+	
+	@Test
+	public void testDeteleNOte() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		
+		
+		WebElement moreElement = driver.findElement(By.cssSelector("#matdiv > div.noteIcon > app-note-icon > div > div:nth-child(9) > a > img"));
+		moreElement.click();
+		
+		sleep(2);
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+		
+		WebElement deleteElement = driver.findElement(By.id("deleteIcon"));
+		deleteElement.click();
+		
+		sleep(3);
+		
+		
+		WebElement sideNavElement = driver.findElement(By.id("sideMenu"));
+		sideNavElement.click();
+		
+		sleep(3);
+		
+		WebElement trashSideNavElement = driver.findElement(By.id("onClickTrash"));
+		trashSideNavElement.click();
+		
+		sleep(3);
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+		
+		
+	}
+	
+	@Test 
+	public void testPinNote() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+		
+		WebElement pinNoteElement = driver.findElement(By.id("pin"));
+		pinNoteElement.click();
+		
+		sleep(2);
+		
+		WebElement element = driver.findElement(By.id("onClickRefresh"));
+		element.click();
+		sleep(10);
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+	}
+	
+	@Test 
+	public void testColorNote() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		sleep(2);
+		screenCapture(new Exception().getStackTrace()[0].getMethodName());
+		WebElement addColorElement = driver.findElement(By.id("addColor"));
+		addColorElement.click();
+		sleep(1);
+		
+		
+		WebElement clickColorElement = driver.findElement(By.id("clickColor"));
+		clickColorElement.click();
+		
+		sleep(2);
+
+		WebElement element = driver.findElement(By.id("onClickRefresh"));
+		element.click();
 		sleep(10);
 		screenCapture(new Exception().getStackTrace()[0].getMethodName());
 	}
 	
 	
+	//Labels
+	
+	@Test
+	public void testCreateLabel() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		WebElement sideNavElement = driver.findElement(By.id("sideMenu"));
+		sideNavElement.click();
+		
+		sleep(2);
+		
+		WebElement editLabelElement = driver.findElement(By.id("onClickEditLabel"));
+		editLabelElement.click();
+		
+		
+		WebElement createLabelElement = driver.findElement(By.id("onClickCreateLabel"));
+		createLabelElement.sendKeys("Test label");
+																	
+		WebElement submitLabelElement = driver.findElement(By.id("onClickSubmitLabel"));
+		submitLabelElement.click();
+		
+		sleep(2);
+		
+		
+		
+		WebElement element = driver.findElement(By.id("onClickRefresh"));
+		element.click();
+		sleep(10);
+		
+		
+		WebElement sideElement = driver.findElement(By.id("sideMenu"));
+		sideElement.click();
+		
+		sleep(5);
+		
+		
+	}
+	
+	@Test
+	public void testEditLabel() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		WebElement sideNavElement = driver.findElement(By.id("sideMenu"));
+		sideNavElement.click();
+		
+		sleep(2);
+		
+		WebElement editLabelElement = driver.findElement(By.id("onClickEditLabel"));
+		editLabelElement.click();
+		
+		
+		WebElement inputLabelElement = driver.findElement(By.id("inputLabel"));
+		inputLabelElement.clear();
+		inputLabelElement.sendKeys("Edited Label");
+																	
+		WebElement clickEditLabelElement = driver.findElement(By.id("clickEdit"));
+		clickEditLabelElement.click();
+		
+
+		sleep(1);
+		
+		WebElement element = driver.findElement(By.id("onClickRefresh"));
+		element.click();
+		sleep(10);
+		
+		
+		WebElement sideElement = driver.findElement(By.id("sideMenu"));
+		sideElement.click();
+		
+		sleep(5);
+	}
 	
 	
+	@Test
+	public void testDeleteLabel() {
+		driver.get("http://localhost:4200/login");
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		emailElement.sendKeys("brijeshkanchan7@gmail.com");
+		
+		sleep(2);
+		
+		WebElement passwordElement = driver.findElement(By.id("pass"));
+		passwordElement.sendKeys("abcd");
+		
+		sleep(2);
+		
+		WebElement submitElement = driver.findElement(By.id("submit"));
+		submitElement.click();
+		
+		
+		sleep(15);
+		
+		driver.manage().window().maximize();
+		
+		WebElement sideNavElement = driver.findElement(By.id("sideMenu"));
+		sideNavElement.click();
+		
+		sleep(2);
+		
+		WebElement editLabelElement = driver.findElement(By.id("onClickEditLabel"));
+		editLabelElement.click();
+		
+		
+		WebElement deleteLabelElement = driver.findElement(By.id("deleteLabel"));
+		deleteLabelElement.click();
+																	
+		
+		sleep(1);
+		
+		WebElement element = driver.findElement(By.id("onClickRefresh"));
+		element.click();
+		sleep(10);
+		
+		
+		WebElement sideElement = driver.findElement(By.id("sideMenu"));
+		sideElement.click();
+		
+		sleep(5);
+	}
 }
